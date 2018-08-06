31c3d3202e98018c3006bb0121bf0e846fa4cdd4
Lucio Paiva <luciopaiva@gmail.com>
2018-08-06 00:17:09 -0300

    Fractal improved Perlin noise

---
+++ fractal-improved-perlin.js
@@ -0,0 +1,30 @@
+
+class FractalImprovedPerlin {
+
+    /**
+     * Generates 3D fractal improved Perlin noise.
+     *
+     * @param {Number} x
+     * @param {Number} y
+     * @param {Number} z
+     * @param {Number} octaves some integer value greater or equal than 1 - more octaves mean more detail
+     * @param {Number} persistence value normally in the range [0, 1[ to scale amplitude down each octave
+     * @param {Number} lacunarity usually 2, scalar to multiply frequency for every next octave
+     * @returns {Number} a value in the range [0, 1[
+     */
+    static noise3d(x, y, z, octaves = 1, persistence = 0.5, lacunarity = 2) {
+        let value = 0;
+        let amplitude = 1;
+        let maxValue = 0;
+        let frequency = 1;
+
+        for (let octave = 0; octave < octaves; octave++) {
+            value += ImprovedPerlin.noise3d(x * frequency, y * frequency, z * frequency) * amplitude;
+            maxValue += amplitude;
+            amplitude *= persistence;
+            frequency *= lacunarity;
+        }
+
+        return value / maxValue;  // normalize it
+    }
+}
---
+++ index.js
@@ -0,0 +1,76 @@
+
+class App {
+
+    constructor() {
+        this.hourglass = document.getElementById("hourglass");
+        this.canvas = document.getElementById("canvas");
+        this.context = this.canvas.getContext("2d");
+
+        this.isRendering = false;
+
+        this.reset();
+
+        this.hourglass.classList.remove("hidden");
+        setTimeout(() => {
+            // postpone to next tick so hourglass can show up
+            this.draw();
+            this.hourglass.classList.add("hidden");
+        }, 1);
+
+        window.addEventListener("resize", this.resize.bind(this));
+    }
+
+    /** Draws a black canvas */
+    reset() {
+        this.canvas.width = window.innerWidth;
+        this.canvas.height = window.innerHeight;
+
+        const ctx = this.context;
+        const imageData = ctx.getImageData(0, 0, this.canvas.width, this.canvas.height);
+        const buffer = imageData.data;
+
+        buffer.fill(0);
+        for (let i = 3; i < buffer.length; i += 4) {
+            buffer[i] = 255;
+        }
+
+        ctx.putImageData(imageData, 0, 0);
+    }
+
+    draw() {
+        this.isRendering = true;
+
+        const ctx = this.context;
+        const imageData = ctx.getImageData(0, 0, this.canvas.width, this.canvas.height);
+        const buffer = imageData.data;
+
+        let x = 0;
+        let y = 0;
+        for (let i = 0; i < this.canvas.width; i++) {
+            for (let j = 0; j < this.canvas.height; j++) {
+                x = i / 256;  // to avoid integer coordinates, which would generate uniform noise
+                y = j / 256;  //
+                const noise = FractalImprovedPerlin.noise3d(x, y, 0, 6, 0.5);
+
+                const bi = 4 * (this.canvas.width * j + i);
+                buffer[bi    ] = 0;
+                buffer[bi + 1] = noise >= 0.55 ? noise * 255 : 0;
+                buffer[bi + 2] = noise < 0.55 ? noise * 255 : 0;
+                buffer[bi + 3] = 255;  // alpha channel
+            }
+        }
+
+        ctx.putImageData(imageData, 0, 0);
+        this.isRendering = false;
+    }
+
+    resize() {
+        this.canvas.width = window.innerWidth;
+        this.canvas.height = window.innerHeight;
+        if (!this.isRendering) {
+            this.draw();
+        }
+    }
+}
+
+new App();
